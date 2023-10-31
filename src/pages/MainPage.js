db069cee733ee6dabbc0371d588cf09c057337e1
savald <savald@taltech.ee>
2023-10-31 19:04:18 +0200

    Loodud Register Page ja täiendatud App.Js. LoginPage.js lisatud direct to register page. Kui kasutaja on sisselogitud siis suunatakse main page ja kui tahab registreerida siis suunatakse regamislehele. Kui ei taha registreerida siis suunatakse login page.

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const MainPage = () => {
+const MainPage = ({ onLogout }) => {
+    const [loggedIn, setLoggedIn] = useState(true);
+
+    const handleLogout = () => {
+        // Simulate a logout action (you can replace this with your actual logout logic)
+        // For now, we'll simulate a logout by changing the logged-in state.
+        setLoggedIn(false);
+        onLogout(); // You can add your actual logout logic here.
+    };
+
     return (
         <div>
-            <h1>Main Page</h1>
-            <p>Welcome to the main page!</p>
+            {loggedIn ? (
+                <div>
+                    <h1>Main Page</h1>
+                    <p>Welcome to the main page of your application.</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            ) : (
+                <div>
+                    <h1>Logged Out</h1>
+                    <p>You've been logged out.</p>
+                </div>
+            )}
         </div>
     );
 };
