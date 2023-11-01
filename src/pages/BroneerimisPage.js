5dac6cf72382a546f1d13cd6a1d6279a85aafd93
Tormi <totulv@taltech.ee>
2023-11-01 14:37:32 +0200

    Lisatud BroneerimisPage.js. MainPage.js lisasin nuppu ridadel 9-11, et saaks broneermis lehele, aga see ei tööta. Et näha broneerimis lehte vahetasin App.js peale sisse logimist maandumislehe BroneerimisPage.js-ks.

---
+++ src/pages/BroneerimisPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BroneerimisPage = () => {
+    return (
+        <div>
+            <h1>Broneerimine</h1>
+            <p>Welcome to broneerimine!</p>
+        </div>
+    );
+};
+
+export default BroneerimisPage;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import BroneerimisPage from "./BroneerimisPage";
 
 const MainPage = () => {
     return (
         <div>
             <h1>Main Page</h1>
             <p>Welcome to the main page!</p>
+            <a href={BroneerimisPage} target={"_blank"}>
+                <button type="button">Broneeri!</button>
+            </a>
         </div>
     );
 };
