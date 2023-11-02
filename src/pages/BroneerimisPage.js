5b4870672c6a3f01092f348eb183ff953328784d
Tormi <totulv@taltech.ee>
2023-11-02 02:52:31 +0200

    Lisasin BroneerimisPage.js Klubi nupud, mis viivad Tallinnas olevatele veebilehtedele, kus saab tenniseväljakuid broneerida. 8-25 line. Peale sisse logimist läheb ikka BroneeimisPage.js peale, et näha muutusi.

--- src/pages/BroneerimisPage.js
+++ src/pages/BroneerimisPage.js
@@ -5,6 +5,24 @@ const BroneerimisPage = () => {
         <div>
             <h1>Broneerimine</h1>
             <p>Welcome to broneerimine!</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>
+                            <a href="https://goldenclub.ee/online-broneerimine/" target="_blank">Klubi 1</a>
+                        </th>
+                        <th>
+                            <a href="https://tallinktennisekeskus.ee/et/tennis/" target="_blank">Klubi 2</a>
+                        </th>
+                        <th>
+                            <a href="https://tenniseklubi.ee/?site#broneerimine" target="_blank">Klubi 3</a>
+                        </th>
+                        <th>
+                            <a href="https://laagritennis.ee/broneeri/" target="_blank">Klubi 4</a>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
