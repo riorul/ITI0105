679820ddcf4e11ccd8434c0dc92772f7c03860c7
RIvo Orulepa <riorul@ttu.ee>
2023-11-16 04:19:42 +0200

    Header with hover, svg and menu. Design is exactly like in Figma.

--- src/assets/MagnifierSVG.js
+++ src/assets/MagnifierSVG.js
@@ -14,15 +14,15 @@ const MagnifierSVG = ({ hovered }) => (
             rx="16.0417"
             ry="16.0417"
             className="magnifier-circle"
-            stroke="white"  // Set the stroke color to white
-            strokeWidth="2"  // Set the stroke width
-            fill="transparent"  // Make the circle transparent, i.e., hollow
+            stroke="white"
+            strokeWidth="2"
+            fill="transparent"
         />
         <path
             d="M45.8334 45.8335L38.9584 38.9585"
             className="magnifier-line"
-            stroke="white"  // Set the stroke color to white
-            strokeWidth="2"  // Set the stroke width
+            stroke="white"
+            strokeWidth="2"
             strokeLinecap="round"
         />
     </svg>
